6cdd5a061055a7894293944a21a7a57697c36062
mukim s <oispuu.joonas@gmail.com>
2023-12-13 14:39:54 +0200

    Removed search

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -25,10 +25,6 @@ export default {
               <router-link class="nav-link" to="/mealorders">Meal Orders</router-link>
             </li>
           </ul>
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
         </div>
       </div>
     </nav> 
